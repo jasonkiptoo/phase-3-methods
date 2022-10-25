936e38faccf1a28dd2b223864e3e9299bcfe26bc
jasonkiptoo <kiptoojason@gmail.com>
2022-10-25 16:02:53 +0300

    COmpleted assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add (x, y)
+    return x + y
+end
+
+def halve (x)
+    if x.class != Integer
+        return nil
+    end
+    return x/2
+end
